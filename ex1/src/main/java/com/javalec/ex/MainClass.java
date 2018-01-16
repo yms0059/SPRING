1307b087b58f9e32294ba343027eac8d77890dfd
윤명식 <yms0059@naver.com>
2018-01-16 16:13:26 +0900

    Spring-DI 2번째

--- ex1/src/main/java/com/javalec/ex/MainClass.java
+++ ex1/src/main/java/com/javalec/ex/MainClass.java
@@ -4,30 +4,14 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainClass {
-
+	
 	public static void main(String[] args) {
-		
-		/*
-		MyCalculator myCalculator = new MyCalculator();
-		myCalculator.setCalculator(new Calculator());
-		
-		myCalculator.setFirstNum(10);
-		myCalculator.setSecondNum(2);
-		
-		myCalculator.add();
-		myCalculator.sub();
-		myCalculator.mul();
-		myCalculator.div();
-		*/
 		
 		String configLocation = "classpath:applicationCTX.xml";
 		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
-		MyCalculator myCalculator = ctx.getBean("myCalculator", MyCalculator.class);
-		
-		myCalculator.add();
-		myCalculator.sub();
-		myCalculator.mul();
-		myCalculator.div();
+		MyInfo myInfo = ctx.getBean("myInfo", MyInfo.class);
+		myInfo.getInfo();
+		ctx.close();
 		
 	}
 	
