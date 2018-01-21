2abaae28ed1fca3467a05ce6e34100d320f87897
윤명식 <yms0059@naver.com>
2018-01-21 14:07:02 +0900

    Spring- 게시판 write 부분 설계

--- spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/command/BListCommand.java
+++ spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/command/BListCommand.java
@@ -1,13 +1,22 @@
 package com.javalec.spring_pjt_board.command;
 
+import java.util.ArrayList;
+
 import org.springframework.ui.Model;
 
-public class BListCommand implements BCommand {
+import com.javalec.spring_pjt_board.dao.BDao;
+import com.javalec.spring_pjt_board.dto.BDto;
+
+public class BListCommand implements BCommand {//command->service로 네이밍 할것
 
 	@Override
 	public void execute(Model model) {
 		// TODO Auto-generated method stub
 
+		BDao dao=new BDao();
+		ArrayList<BDto>dtos=dao.list();
+		
+		model.addAttribute("list",dtos);
 		
 	}
 
--- spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
+++ spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
@@ -1,16 +1,104 @@
 package com.javalec.spring_pjt_board.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.jsp.jstl.sql.Result;
+import javax.sql.DataSource;
 
 import com.javalec.spring_pjt_board.dto.BDto;
 
 public class BDao {
 
-	
-	public ArrayList<BDto>list() //데이터객체를 가져오는 거겠죠? DTO를 만들어 이용
+	DataSource dataSource;
+
+	public void write(String bName, String bTitle, String bContent) {
+		Connection connection = null; // 초기화는 아니고 선언만
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = dataSource.getConnection();
+			String query = "Insert into mvc_board(bId,bName,bTitle,bContent,bHit,bGroup,bStep,bIndent)values(mvc_board_seq.nextval,?,?,?,0,mvc_board_seq.currval,0,0)";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, bName);
+			preparedStatement.setString(2, bTitle);
+			preparedStatement.setString(3, bContent);
+			
+			int rn = preparedStatement.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public BDao() {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ArrayList<BDto> list() // 데이터객체를 가져오는 거겠죠? DTO를 만들어 이용
 	{
-		ArrayList<BDto>dtos=null;
-		
+		ArrayList<BDto> dtos = new ArrayList<BDto>();
+		Connection connection = null; // 초기화는 아니고 선언만
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			connection = dataSource.getConnection();
+			String query = "select bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent from mvc_board order by bGroup desc, bStep asc";
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				int bId = resultSet.getInt("bId");
+				String bName = resultSet.getString("bName");
+				String bTitle = resultSet.getString("bTitle");
+				String bContent = resultSet.getString("bContent");
+				Timestamp bDate = resultSet.getTimestamp("bDate");
+				int bHit = resultSet.getInt("bHit");
+				int bGroup = resultSet.getInt("bGroup");
+				int bStep = resultSet.getInt("bStep");
+				int bIndent = resultSet.getInt("bIndent");
+
+				BDto dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+				dtos.add(dto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+				if (preparedStatement != null)
+					preparedStatement.close();
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+			}
+
+		}
+
 		return dtos;
 	}
 }
